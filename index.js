527f775d06291803795298f00f0076fe845e296f
Kevin Jung <kev.jung.ii@gmail.com>
2021-05-02 12:11:45 -0400

    commit tree c29656a13ac4e70f495371e486665309567397be

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+const pi = Math.PI
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+
+  get diameter() {
+    return this.radius * 2
+  }
+
+  get circumference() {
+    return this.radius * 2 * pi
+  }
+
+  get area() {
+    return  pi * this.radius ** 2
+  }
+
+  set diameter(num) {
+    this.radius = num / 2
+  }
+
+  set circumference(num) {
+    this.radius = num / 2 / pi
+  }
+
+  set area(num) {
+    this.area = Math.sqrt(num / 2)
+  }
+}
